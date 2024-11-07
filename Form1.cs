a04bae35a96b8b53a309dab186ecb90778750621
Elmir Rzazade <124444106+elmirshidev@users.noreply.github.com>
2024-11-07 14:26:12 +0400

    Added files via upload

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<User> users = new List<User>()
+            {
+                new User(1,"Elmir", "elmir@nasa.com" ,  "elmir123"),
+                new User(2,"Nihat", "nihat@nasa.com" ,  "nihat123"),
+                new User(3,"Nurlan", "nurlan@nasa.com" ,  "nurlan123"),
+                new User(4,"Amin", "amin@nasa.com" ,  "amin123"),
+            };
+            var username = textBox1.Text;
+            var password = textBox2.Text;
+            if(username.Trim() == "" || password.Trim() == "")
+            {
+                MessageBox.Show("Fill all fields please!");
+                return;
+            }
+            var foundUser = users.Find(u => u.Name == username);
+            if(foundUser == null)
+            {
+                MessageBox.Show("Username is incorrect!");
+                return;
+            }
+            if(foundUser.Password != password)
+            {
+                MessageBox.Show("Password is incorrect!");
+                return;
+            }
+            MessageBox.Show("Correct credentials!");
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class User
+    {
+        private int _id;
+        private string _name;
+        private string _email;
+        private string _password;
+        public string Password { get { return _password; } }
+        public int Id { get { return _id;} }
+        public string Name { get { return _name;} }
+        public string Email { get { return _email;} }
+        public User(int id,string name,string email,string password)
+        {
+            this._id = id;
+            this._name = name;
+            this._email = email;
+            this._password = password;
+        }
+    }
+}
